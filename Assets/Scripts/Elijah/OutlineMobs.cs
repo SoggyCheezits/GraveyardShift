0aed69a88ca54bbb27228dbd819d2f5b0a525e70
Elijah-Pasimio <25elijpasimio@students.mvsd320.org>
2023-11-18 08:26:18 -0800

    Tuned

--- Assets/Scripts/Elijah/OutlineMobs.cs
+++ Assets/Scripts/Elijah/OutlineMobs.cs
@@ -21,14 +21,55 @@ public class OutlineMobs : MonoBehaviour
         allies = GameObject.FindGameObjectsWithTag("Ally");
         enemies = GameObject.FindGameObjectsWithTag("Enemy");
 
+        AddOutline();
+    }
+
+    public void AddOutline()
+    {
         foreach (GameObject ally in allies)
         {
-            Instantiate(allyOutline, ally.transform.position, ally.transform.rotation).transform.parent = ally.transform;
+            bool hasOutline = false;
+           
+
+            Rotating[] children = ally.GetComponentsInChildren<Rotating>();
+
+            foreach(Rotating child in children)
+            {
+                if(child.gameObject.CompareTag("Outline"))
+                {
+                    hasOutline = true;
+                }
+            }
+
+            if (!hasOutline)
+            {
+                Instantiate(allyOutline, ally.transform.position, ally.transform.rotation).transform.parent = ally.transform;
+            }
+
+
+
         }
 
         foreach (GameObject enemy in enemies)
         {
-            Instantiate(enemyOutline, enemy.transform.position, enemy.transform.rotation).transform.parent = enemy.transform;
+            bool hasOutline = false;
+
+            Rotating[] children = enemy.GetComponentsInChildren<Rotating>();
+
+            foreach (Rotating child in children)
+            {
+                if (child.gameObject.CompareTag("Outline"))
+                {
+                    hasOutline = true;
+                }
+            }
+
+            if (!hasOutline)
+            {
+                hasOutline = true;
+                Instantiate(enemyOutline, enemy.transform.position, enemy.transform.rotation).transform.parent = enemy.transform;
+
+            }
         }
     }
 }
